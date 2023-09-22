1b93100cb97484bdb4bdcb4c07b5db5597ec2da9
Rick Gladwin <rick@iddesign.ca>
2023-09-22 17:36:18 -0400

    add build_excavator_address and spec

--- app/controllers/api/v1/ticket_data_controller.rb
+++ app/controllers/api/v1/ticket_data_controller.rb
@@ -1,17 +1,13 @@
-# require_relative '../../application_controller'
-# require_relative '../'
+# Handles incoming ticket data
+
 class Api::V1::TicketDataController < ApplicationController
   # disable CSRF protection (use API token in production)
   protect_from_forgery with: :null_session
 
   def input
-    # render inline: "API endpoint"
     # parse input json
     input_json = JSON.parse(request.raw_post)
-    # render inline: request.raw_post
-    # render inline: input_json
-    # render inline: input_json['ContactCenter']
-    render inline: input_json['ExcavationInfo']['DigsiteInfo']['WellKnownText']
+    # render inline: input_json['ExcavationInfo']['DigsiteInfo']['WellKnownText']
 
     # validate input data
     # create Ticket and Excavator from input data
@@ -23,28 +19,45 @@ class Api::V1::TicketDataController < ApplicationController
       response_due_date_time:        input_json['DateTimes']['ResponseDueDateTime'],
       primary_service_area_code:     input_json['ServiceArea']['PrimaryServiceAreaCode']['SACode'],
       additional_service_area_codes: input_json['ServiceArea']['AdditionalServiceAreaCodes']['SACode'],
-    # digsite_info: input_json['DigsiteInfo']['WellKnownText'],
+      digsite_info: well_known_text_to_polygon(input_json['ExcavationInfo']['DigsiteInfo']['WellKnownText']),
     )
-    # new_ticket = Ticket.create(
-    #   request_number: input_json['RequestNumber'],
-    #   sequence_number: input_json['SequenceNumber'],
-    #   request_type: input_json['Normal'],
-    #   request_action: input_json['Restake'],
-    #   response_due_date_time: input_json['DateTimes']['ResponseDueDateTime'],
-    #   primary_service_area_code: input_json['ServiceArea']['PrimaryServiceAreaCode']['SACode'],
-    #   additional_service_area_codes: input_json['ServiceArea']['AdditionalServiceAreaCodes']['SACode'],
-    # # digsite_info: input_json['DigsiteInfo']['WellKnownText'],
-    #   )
-    # render inline: "created new ticket: #{new_ticket.request_number}"
+    # render inline: "new Ticket created: #{new_ticket.to_json}", status: 201
+
+    new_excavator = Excavator.create(
+      company_name: input_json['Excavator']['CompanyName'],
+      address: build_excavator_address(input_json),
+      crew_on_site: input_json['Excavator']['CrewOnsite'],
+      ticket_id: new_ticket.id
+    )
+
+    render inline: "new Ticket and Excavator created: #{new_ticket.to_json}, #{new_excavator.to_json}", status: 201
   end
 
   # converts value format from json data to PostgreSQL polygon format
   # example input:
   # POLYGON((-81.13390268058475 32.07206917625161,-81.14660562247929 32.04064386441295,-81.08858407706913 32.02259853170128))
+  # example output:
+  # ( ( x1 , y1 ) , ... , ( xn , yn ) )
   def well_known_text_to_polygon(well_known_text)
-    # guard format
-    input_regex = /POLYGON\(\(.*\)\)/
+    # validate format
+    validation_result = validate_well_known_text(well_known_text)
+    if validation_result[:status] == false
+      raise "well_known_text validation for #{well_known_text} failed: #{validation_result[:message]}"
+    end
 
+    coordinates = '('
+
+    # parse value
+    well_known_text_array = well_known_text
+                              .delete_prefix('POLYGON((')
+                              .delete_suffix('))')
+                              .split(',')
+    well_known_text_array.each do |lat_long_string|
+      lat_long = lat_long_string.split(' ')
+      coordinates += "(#{lat_long[0]},#{lat_long[1]}),"
+    end
+    coordinates.delete_suffix!(',')
+    coordinates + ')'
   end
 
   def validate_well_known_text(well_known_text)
@@ -52,28 +65,14 @@ class Api::V1::TicketDataController < ApplicationController
     if value_regex.match(well_known_text)
       return { :status => true, :message => '' }
     else
-      return { :status => false, :message => "well_known_text value does not match regex pattern #{value_regex}"}
+      return { :status => false, :message => "well_known_text value does not match regex pattern (minimum 3 lat/long pairs, no padding spaces) #{value_regex}"}
     end
-    return { :status => false, :message => 'could not validate well_known_text'}
   end
-end
 
-if __FILE__ == $0
-  require_relative '../../../../config/global_config'
-  exit unless Config.run_file_level_unit_tests
-
-  require_relative '../../application_controller'
-  test_ticket_data_controller = Api::V1::TicketDataController.new
-
-  # file-level unit tests
-  # These are development artifacts left in place as debugging/refactoring tools
-  # or deleted in production code. These tests are more minimal and informal than
-  # RSpec tests.
-
-  # invalidates well_known_text in the wrong format
-  test_value               = 'INCORRECT((12345 345567, 234234 -56.788))'
-  expected_validation_bool = false
-  raise "failed to invalidate well_known_text" unless test_ticket_data_controller.validate_well_known_text(test_value)[0] == expected_validation_bool
-
-  puts "file level unit tests passed"
+  def build_excavator_address(input_json)
+    "#{input_json['Excavator']['Address']}, "+
+      "#{input_json['Excavator']['City']}, "+
+      "#{input_json['Excavator']['State']}, "+
+      "#{input_json['Excavator']['Zip']}"
+  end
 end
--- spec/controllers/api/v1/ticket_data_controller_spec.rb
+++ spec/controllers/api/v1/ticket_data_controller_spec.rb
@@ -3,39 +3,78 @@
 describe Api::V1::TicketDataController, "#validate_well_known_text" do
   ticket_data_controller = Api::V1::TicketDataController.new
 
-  it "invalidates well_known_text missing POLYGON prefix" do
-    test_value = 'INCORRECT((12345 345567, 234234 -56.788))'
-    result = ticket_data_controller.validate_well_known_text(test_value)
-    expect(result[:status]).to eq false
+  describe "validate_well_known_text" do
+    it "invalidates well_known_text missing POLYGON prefix" do
+      test_value = 'INCORRECT((12345 345567, 234234 -56.788))'
+      result = ticket_data_controller.validate_well_known_text(test_value)
+      expect(result[:status]).to eq false
+    end
+
+    it "invalidates well_known_text with fewer than 3 polygon points" do
+      test_value = 'POLYGON((58.835 -123.987,55.2 -125.67))'
+      result = ticket_data_controller.validate_well_known_text(test_value)
+      expect(result[:status]).to eq false
+    end
+
+    it "invalidates well_known_text with extra leading spaces" do
+      test_value = 'POLYGON(( 56.23 -123.456,58.835 -123.987,55.2 -125.67))'
+      result = ticket_data_controller.validate_well_known_text(test_value)
+      expect(result[:status]).to eq false
+    end
+
+    it "invalidates well_known_text with extra trailing spaces" do
+      test_value = 'POLYGON((56.23 -123.456,58.835 -123.987,55.2 -125.67 ))'
+      result = ticket_data_controller.validate_well_known_text(test_value)
+      expect(result[:status]).to eq false
+    end
+
+    it "invalidates well_known_text with spaces after commas" do
+      test_value = 'POLYGON((56.23 -123.456, 58.835 -123.987, 55.2 -125.67))'
+      result = ticket_data_controller.validate_well_known_text(test_value)
+      expect(result[:status]).to eq false
+    end
+
+    it "validates well_known_text" do
+      test_value = 'POLYGON((56.23 -123.456,58.835 -123.987,55.2 -125.67))'
+      result = ticket_data_controller.validate_well_known_text(test_value)
+      expect(result[:status]).to eq true
+    end
   end
 
-  it "invalidates well_known_text with fewer than 3 polygon points" do
-    test_value = 'POLYGON((58.835 -123.987,55.2 -125.67))'
-    result = ticket_data_controller.validate_well_known_text(test_value)
-    expect(result[:status]).to eq false
+  describe "well_known_text_to_polygon" do
+    it "throws an exception given invalid input" do
+      test_value = 'INCORRECT((12345 345567, 234234 -56.788))'
+      expect { ticket_data_controller.well_known_text_to_polygon(test_value) }.to raise_error(RuntimeError)
+    end
+
+    it "converts valid input to polygon data format" do
+
+      # POLYGON((-81.13390268058475 32.07206917625161,-81.14660562247929 32.04064386441295,-81.08858407706913 32.02259853170128))
+      # example output:
+      # ( ( x1 , y1 ) , ... , ( xn , yn ) )
+      test_value = 'POLYGON((56.23 -123.456,58.835 -123.987,55.2 -125.67))'
+      expected_result = '((56.23,-123.456),(58.835,-123.987),(55.2,-125.67))'
+      result = ticket_data_controller.well_known_text_to_polygon(test_value)
+      expect(result).to eq expected_result
+    end
   end
 
-  it "invalidates well_known_text with extra leading spaces" do
-    test_value = 'POLYGON(( 56.23 -123.456,58.835 -123.987,55.2 -125.67))'
-    result = ticket_data_controller.validate_well_known_text(test_value)
-    expect(result[:status]).to eq false
+  describe "build_excavator_address" do
+    it "builds excavator address string from json data" do
+      test_data = '{
+        "Excavator":{
+          "CompanyName":"John Doe CONSTRUCTION",
+          "Address":"555 Some RD",
+          "City":"SOME PARK",
+          "State":"ZZ",
+          "Zip":"55555"
+        }
+      }'
+      test_json = JSON.parse(test_data)
+      expected_result = '555 Some RD, SOME PARK, ZZ, 55555'
+      result = ticket_data_controller.build_excavator_address(test_json)
+      expect(result).to eq expected_result
+    end
   end
 
-  it "invalidates well_known_text with extra trailing spaces" do
-    test_value = 'POLYGON((56.23 -123.456,58.835 -123.987,55.2 -125.67 ))'
-    result = ticket_data_controller.validate_well_known_text(test_value)
-    expect(result[:status]).to eq false
-  end
-
-  it "invalidates well_known_text with spaces after commas" do
-    test_value = 'POLYGON((56.23 -123.456, 58.835 -123.987, 55.2 -125.67))'
-    result = ticket_data_controller.validate_well_known_text(test_value)
-    expect(result[:status]).to eq false
-  end
-
-  it "validates well_known_text" do
-    test_value = 'POLYGON((56.23 -123.456,58.835 -123.987,55.2 -125.67))'
-    result = ticket_data_controller.validate_well_known_text(test_value)
-    expect(result[:status]).to eq true
-  end
 end
