000af45eb255c8c244765b35ac5eb30365b88c2d
Rick Gladwin <rick@iddesign.ca>
2023-09-22 12:59:58 -0400

    add global config

---
+++ config/global_config.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Global config
+# Accessible from any file
+# Can be used to set global variables in main/entrypoint file
+
+module Config
+  def self.run_file_level_unit_tests = true
+end
